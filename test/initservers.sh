3d7d55bf574f6973fb9b4c968f0f03d868768987
Notis Mastrandrikos <pmastrandrikos@gmail.com>
2022-03-18 17:48:33 +0200

    add test server logs

--- test/initservers.sh
+++ test/initservers.sh
@@ -1,3 +1,3 @@
-php -S localhost:3000 website.php > /dev/null 2>&1 &
-php -S localhost:3001 laravelWebsite.php > /dev/null 2>&1 &
-php -S localhost:3002 symfonyWebsite.php > /dev/null 2>&1 &
+php -S localhost:3000 website.php > ./website-log 2>&1 &
+php -S localhost:3001 laravelWebsite.php  > ./laravelWebsite-log 2>&1 &
+php -S localhost:3002 symfonyWebsite.php > ./symfonyWebsite-log 2>&1 &
